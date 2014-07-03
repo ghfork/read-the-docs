f59ef1b2de5f8d931afef2c4e047b2b0f10ec086
yilin.w <revetice@gmail.com>
2014-07-03 14:15:45 +0800

    Modify index

---
+++ _build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{index:0,search:0,modul:0,content:0,test:1,page:0},objtypes:{},objnames:{},filenames:["index","vim/first"],titles:["Welcome to ReadTheDocs&#8217;s documentation!","&lt;no title&gt;"],objects:{},titleterms:{welcom:0,readthedoc:0,document:0,indic:0,tabl:0}})
